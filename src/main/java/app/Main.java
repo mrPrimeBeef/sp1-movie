58e406d8837db2b763aa67998beeea3a1f3b2d64
Rolf <158441656+mrPrimeBeef@users.noreply.github.com>
2025-02-25 11:43:37 +0100

    genres persistet

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,6 +3,8 @@ package app;
 import java.util.Comparator;
 import java.util.List;
 
+import app.daos.GenreeDao;
+import app.entities.Genree;
 import jakarta.persistence.EntityManagerFactory;
 
 import app.config.HibernateConfig;
@@ -13,11 +15,16 @@ public class Main {
     public static void main(String[] args) {
 
         EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+        GenreeDao genreeDao = GenreeDao.getInstance(emf);
 
         List<Movie> movies = TmdbService.getDanishMoviesSince2020();
         movies.stream()
                 .sorted(Comparator.comparing(Movie::getReleaseDate))
                 .forEach(System.out::println);
+
+        TmdbService.GenresResponseDto list = TmdbService.getAllGenres();
+        List<Genree> list2 = TmdbService.getGenres(list.genres());
+        list2.forEach(genreeDao::create);
 
     }
 }
